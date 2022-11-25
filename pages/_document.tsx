47fc30ffb1162d90e021c3f85d338a724774e9d9
esrafilelahi <essinho1121@gmail.com>
2022-11-25 11:13:42 +0330

    fix _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,26 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx)
-
-    return initialProps
+    return { ...initialProps }
   }
 
-  // render() {
-  //   return (
-  //     <Html>
-  //       <Head>
-  //         <link rel="preconnect" href="https://fonts.googleapis.com" />
-  //         <link rel="preconnect" href="https://fonts.gstatic.com" />
-  //         <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet" />
-  //       </Head>
-  //       <body>
-  //         <Main />
-  //         <NextScript />
-  //       </body>
-  //     </Html>
-  //   )
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
