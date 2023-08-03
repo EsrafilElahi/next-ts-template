c0687c90ceebcc52c3a5c932c3ea69fb9002424b
Esrafil <essinho1121@gmail.com>
2023-08-03 22:01:25 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   //     {
   //       source: 'register',
   //       destination: `register`,
-  //     },
+  //     }, 
   //     {
   //       source: 'login',
   //       destination: `login`,
